7ca0cd746f6546c819b0e30cfefbea37f552d81b
dildarali <dildar.ali@qordata.com>
2020-10-07 17:53:34 +0500

    End of Session 6

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,13 +11,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MessagesComponent } from './members/messages/messages.component';
+import { ListsComponent } from './members/lists/lists.component';
 
+import{ToastrModule} from 'ngx-toastr';
+import { SharedModule } from './_modules/shared.module';
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    MessagesComponent,
+    ListsComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +35,7 @@ import { RegisterComponent } from './register/register.component';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AccountService } from '../_services/account.service';
 import {User} from '../_models/user'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +13,7 @@ export class NavComponent implements OnInit {
 model: any={}
 currentUser$:Observable<User>;
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router) { }
 
   ngOnInit(): void {
     
@@ -21,11 +22,13 @@ currentUser$:Observable<User>;
 Login(){
   console.log("this is response-Before "+this.model);
   this.accountService.login(this.model).subscribe(response=> {
-    console.log(response);
+    this.router.navigateByUrl('/members');
+    console.log("login res is:"+response);
   },error=>{console.log(error)});
 
 }
 logout(){
   this.accountService.logout();
+  this.router.navigateByUrl('/');
 }
 }
